02074517d31cab041ef4ad067295733bd2519418
haein Oh <badarangdev@gmail.com>
2024-10-23 20:13:00 +0900

    Delete Unit/BT directory

--- Unit/BT/SharedUnit.cs
+++
@@ -1,7 +0,0 @@
-using BehaviorDesigner.Runtime;
-
-[System.Serializable]
-    public class SharedUnit : SharedVariable<Unit>
-    {
-        public static implicit operator SharedUnit(Unit value) { return new SharedUnit { Value = value }; }
-    }
