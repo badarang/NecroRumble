396c07f2145f7dff9897bfd7fbc3564b013279fa
haein Oh <badarangdev@gmail.com>
2024-10-23 20:07:19 +0900

    Delete DataScripts/SpearManUnitPairData.cs

--- DataScripts/SpearManUnitPairData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Data/Unit/" + nameof(SpearManUnitPairData), fileName = nameof(SpearManUnitPairData))]
-public class SpearManUnitPairData : PairUnitData
-{
-}
